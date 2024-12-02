d4832e5ecef42b9b25bdb13d73c80db52c815d44
Dylan Munyard <dylanmunyard@sixpivot.com.au>
2024-12-02 21:52:29 +1000

    Template

--- src/aoc/x.ts
+++ src/aoc/x.ts
@@ -5,6 +5,10 @@ const solution = async () : Promise<{ 1: number | null, 2: number | null }> => {
 	const asset = read(puzzle);
 	const input = (await asset.text()).split("\n");
 
+	input.forEach(row => {
+		const line = row.trim().split(/\s+/).map(Number);
+	});
+
 	await new Promise(resolve => setTimeout(resolve, Math.floor(Math.random() * (10000 - 2000 + 1) + 2000)));
 
 	return {
